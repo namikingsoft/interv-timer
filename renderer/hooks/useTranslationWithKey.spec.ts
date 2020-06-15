91b51ed23ae29ff1809dad3e24a541ddb480504e
Tsubasa Namiki <namikingsoft@gmail.com>
2020-06-16 00:37:37 +0900

    feat: impl useTranslationWithKey

---
+++ renderer/hooks/useTranslationWithKey.spec.ts
@@ -0,0 +1,17 @@
+import { renderHook } from '@testing-library/react-hooks'
+import { useTranslationWithKey } from './useTranslationWithKey'
+
+describe('useLapTimerReducer', () => {
+  it('should behave lap timer', () => {
+    const {
+      result: {
+        current: { t, k },
+      },
+    } = renderHook(() => useTranslationWithKey())
+
+    expect(k.total).toBe('total')
+    expect(k.margin).toBe('margin')
+    expect(t(k.total)).toBe('total') // TODO: Total in case of en
+    expect(t(k.margin)).toBe('margin') // TODO: Margin in case of en
+  })
+})
