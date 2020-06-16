9799a68bf892208f66ca751e99f801672dc6a64b
Tsubasa Namiki <namikingsoft@gmail.com>
2020-06-16 23:30:52 +0900

    feat: incorporate epics to store

--- renderer/store.ts
+++ renderer/store.ts
@@ -1,6 +1,18 @@
-import { combineReducers, createStore, Store } from 'redux'
+import { applyMiddleware, combineReducers, createStore, Store } from 'redux'
+import { createEpicMiddleware, combineEpics } from 'redux-observable'
 import { timer } from './modules/timer/reducer'
+import { setting } from './modules/setting/reducer'
+import * as settingEpics from './modules/setting/epic'
 import { State, Action } from './modules/type'
 
-export const createReduxStore = (): Store<State, Action> =>
-  createStore(combineReducers({ timer }))
+const rootEpic = combineEpics(...Object.values(settingEpics))
+
+export const createReduxStore = (): Store<State, Action> => {
+  const epicMiddleware = createEpicMiddleware()
+  const store = createStore(
+    combineReducers({ timer, setting }),
+    applyMiddleware(epicMiddleware),
+  )
+  epicMiddleware.run(rootEpic)
+  return store
+}
