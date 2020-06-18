f2222fd44efca918c1a10d7a68ed886b8912d154
Tsubasa Namiki <namikingsoft@gmail.com>
2020-06-18 18:33:32 +0900

    feat: add new input locales

--- renderer/locales/en.ts
+++ renderer/locales/en.ts
@@ -2,5 +2,10 @@ export const en = {
   translation: {
     total: 'Total',
     margin: 'Margin',
+    lapInfoListPlaceholder:
+      'Please enter lap of label and second.\n\ne.g.\n\nAgenda1,60\nAgenda2,180',
+    agendaList: 'Agenda List',
+    avoidFinishedAgenda: 'Avoid Finished Agenda',
+    backgroundTransparentRate: 'Background Transparent Rate',
   },
 }
