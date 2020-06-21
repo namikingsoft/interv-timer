a5d12a46ae12ee6e7f6c351ccda7d042a0792e8b
Tsubasa Namiki <namikingsoft@gmail.com>
2020-06-21 13:27:24 +0900

    fix: calc delta second from AudioContext

--- renderer/hooks/useIntervalByAudioContext.ts
+++ renderer/hooks/useIntervalByAudioContext.ts
@@ -1,26 +1,32 @@
 import React from 'react'
 
+interface CallbackArg {
+  elapsedSecond: number
+  deltaSecond: number
+}
+
 export const useIntervalByAudioContext = (
   intervalSecond: number,
-  callback: (elapsedSecond: number) => void,
+  intervalCallback: (arg: CallbackArg) => void,
 ): void => {
   React.useEffect(() => {
-    let prevElapsedSecond = 0
+    let prevCallbackSecond = 0
     let animationId: number
     // @ts-expect-error webkitAudioContext not defined
     const AppAudioContext = window.AudioContext || window.webkitAudioContext
     const audioContext = new AppAudioContext()
     const step = () => {
-      if (
-        audioContext.currentTime % intervalSecond <
-        prevElapsedSecond % intervalSecond
-      ) {
-        callback(audioContext.currentTime)
+      if (intervalSecond < audioContext.currentTime - prevCallbackSecond) {
+        const currentTime = audioContext.currentTime
+        intervalCallback({
+          elapsedSecond: currentTime,
+          deltaSecond: currentTime - prevCallbackSecond,
+        })
+        prevCallbackSecond = currentTime
       }
-      prevElapsedSecond = audioContext.currentTime
       animationId = window.requestAnimationFrame(step)
     }
     animationId = window.requestAnimationFrame(step)
     return () => window.cancelAnimationFrame(animationId)
-  }, [intervalSecond, callback])
+  }, [intervalSecond, intervalCallback])
 }
