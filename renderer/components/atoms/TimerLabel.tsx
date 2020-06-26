4fe36f08e6c5fc70c6493d1ed263dd1d24e78ca1
Tsubasa Namiki <namikingsoft@gmail.com>
2020-06-27 08:09:58 +0900

    refactor: move func to timer module

--- renderer/components/atoms/TimerLabel.tsx
+++ renderer/components/atoms/TimerLabel.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import classnames from 'classnames'
 import { makeStyles, createStyles } from '@material-ui/core/styles'
+import { calcTimerLabelFromRemainSecond } from '../../modules/timer/util'
 
 interface Props {
   className?: string
@@ -15,16 +16,9 @@ const useStyles = makeStyles(() =>
   }),
 )
 
-const zeroPad2 = (x: number) => String(x).padStart(2, '0')
-
 export const TimerLabel: React.FC<Props> = ({ className, remainSecond }) => {
   const classes = useStyles({})
-  const remainSecondAbs = Math.abs(remainSecond)
-  const remainMinute = remainSecondAbs / 60
-  const remainHour = remainMinute / 60
-  const second = zeroPad2(Math.round(remainSecondAbs % 60)) // NOTE: 58.999 => 59 not 58
-  const minute = zeroPad2(Math.floor(remainMinute % 60))
-  const hour = zeroPad2(Math.floor(remainHour))
+  const { hour, minute, second } = calcTimerLabelFromRemainSecond(remainSecond)
 
   return (
     <div
--- renderer/modules/timer/util.ts
+++ renderer/modules/timer/util.ts
@@ -19,3 +19,24 @@ export const parseTextToAgendaList = (text: string): Agenda[] => {
         second: Number.isNaN(Number(second)) ? 0 : Number(second),
       }))
 }
+
+interface TimerLabel {
+  hour: string
+  minute: string
+  second: string
+}
+
+const zeroPad2 = (x: number) => String(x).padStart(2, '0')
+
+export const calcTimerLabelFromRemainSecond = (
+  remainSecond: number,
+): TimerLabel => {
+  const remainSecondAbs = Math.abs(remainSecond)
+  const remainMinute = remainSecondAbs / 60
+  const remainHour = remainMinute / 60
+  return {
+    second: zeroPad2(Math.round(remainSecondAbs % 60)), // NOTE: 58.999 => 59 not 58
+    minute: zeroPad2(Math.floor(remainMinute % 60)),
+    hour: zeroPad2(Math.floor(remainHour)),
+  }
+}
