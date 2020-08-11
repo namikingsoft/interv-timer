d8750ca4d20d4b45208b800abe01b4085188f11b
Tsubasa Namiki <namikingsoft@gmail.com>
2020-08-12 06:34:36 +0900

    refactor: renderer components

--- renderer/components/atoms/TimerLabel.tsx
+++ renderer/components/atoms/TimerLabel.tsx
@@ -1,34 +1,32 @@
 import React from 'react'
-import classnames from 'classnames'
 import { makeStyles, createStyles } from '@material-ui/core/styles'
-import { calcTimerLabelFromRemainSecond } from '../../modules/timer/util'
+import classnames from 'classnames'
 
 interface Props {
   className?: string
-  remainSecond: number
+  children: React.ReactNode
   'data-testid'?: string
 }
 
 const useStyles = makeStyles(() =>
   createStyles({
     root: {
-      display: 'inline-block',
+      fontSize: 13,
+      marginBottom: -5,
     },
   }),
 )
 
 export const TimerLabel: React.FC<Props> = ({
   className,
-  remainSecond,
-  'data-testid': dataTestId,
+  children,
+  'data-testid': testId,
 }) => {
   const classes = useStyles({})
-  const { hour, minute, second } = calcTimerLabelFromRemainSecond(remainSecond)
 
   return (
-    <div
-      className={classnames(classes.root, className)}
-      data-testid={dataTestId}
-    >{`${hour}:${minute}:${second}`}</div>
+    <div className={classnames(className, classes.root)} data-testid={testId}>
+      {children}
+    </div>
   )
 }
---
+++ renderer/components/molecules/TimerInfo.tsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import { makeStyles, createStyles } from '@material-ui/core/styles'
+import classnames from 'classnames'
+import { TimerLabel } from '../atoms/TimerLabel'
+import { TimerValue } from '../atoms/TimerValue'
+import { HurryAnimationText } from '../atoms/HurryAnimationText'
+
+interface Props {
+  label: string
+  remainSecond: number
+  isActive?: boolean
+  disabled?: boolean
+  className?: string
+  'data-testid'?: string
+}
+
+const textBorderColorActive = '#36f'
+const textBorderColorExpired = '#f00'
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    root: {
+      marginLeft: 14,
+      userSelect: 'none',
+    },
+    active: {
+      textShadow: `${textBorderColorActive} 1px 1px 0, ${textBorderColorActive} -1px -1px 0, ${textBorderColorActive} -1px 1px 0, ${textBorderColorActive} 1px -1px 0, ${textBorderColorActive} 0px 1px 0, ${textBorderColorActive}  0 -1px 0, ${textBorderColorActive} -1px 0 0, ${textBorderColorActive} 1px 0 0`,
+    },
+    expired: {
+      color: '#000',
+      textShadow: `${textBorderColorExpired} 1px 1px 0, ${textBorderColorExpired} -1px -1px 0, ${textBorderColorExpired} -1px 1px 0, ${textBorderColorExpired} 1px -1px 0, ${textBorderColorExpired} 0px 1px 0, ${textBorderColorExpired}  0 -1px 0, ${textBorderColorExpired} -1px 0 0, ${textBorderColorExpired} 1px 0 0`,
+    },
+    disabled: {
+      opacity: 0.25,
+    },
+  }),
+)
+
+export const TimerInfo: React.FC<Props> = ({
+  label,
+  remainSecond,
+  isActive,
+  disabled,
+  className,
+  'data-testid': testId,
+}) => {
+  const classes = useStyles({})
+
+  return (
+    <div
+      className={classnames(
+        className,
+        classes.root,
+        disabled && classes.disabled,
+      )}
+      data-testid={testId}
+    >
+      <TimerLabel className={classnames(isActive && classes.active, 'label')}>
+        {label}
+      </TimerLabel>
+      <HurryAnimationText
+        hurry={isActive && remainSecond > 10 && remainSecond < 30}
+        hurryUp={isActive && remainSecond >= 0 && remainSecond <= 10}
+      >
+        <TimerValue
+          remainSecond={remainSecond}
+          className={classnames(remainSecond < 0 && classes.expired, 'value')}
+        />
+      </HurryAnimationText>
+    </div>
+  )
+}
